67de845eb4c282ec97714a7823d252f0a246252e
Vitor Gabriel <vitor.grs2004@gmail.com>
2024-11-10 11:24:38 -0300

    Update getProjects.js

--- api/getProjects.js
+++ api/getProjects.js
@@ -1,7 +1,23 @@
 const utils = require('../util.js');
 
 module.exports = (req, res) => {
-    if (req.method === 'GET') {
+    const allowedOrigins = ['https://ncaufma.github.io', 'http://localhost:4000'];
+    const origin = req.headers.origin;
+  
+    // Verifique se a origem da solicitação está na lista de origens permitida
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+      res.setHeader('Access-Control-Allow-Origin', ''); // Define como vazio para outras origens
+    }
+  
+    res.setHeader('Access-Control-Allow-Methods', 'GET'); // Permite apenas o método GET
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Permite apenas cabeçalhos específicos
+  
+    if (req.method === 'OPTIONS') {
+      // Responde a preflight requests (opcional para suportar CORS com métodos HTTP seguros)
+      res.status(200).end();
+    } else if (req.method === 'GET') {
         const doc = utils.getAllProjectsInfo();
         if (doc) {
             res.status(200).json({ message: doc });
